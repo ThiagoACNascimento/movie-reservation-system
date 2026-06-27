0d88eef32b237a82ccff92bebe884772f67d325f
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-06-27 16:57:13 -0400

    chore: add `expandVariales` to connect with `dotenv-expand`

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { StatusModule } from './modules/status/status.module';
       envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
       ignoreEnvFile: process.env.NODE_ENV === 'production' ? true : false,
+      expandVariables: true,
     }),
     StatusModule,
   ],
