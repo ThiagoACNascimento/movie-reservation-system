df64907597f7f9862acd840d3d36ed9c467b412c
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-06-29 18:39:43 -0400

    feat: create initial `create` user method

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from '@/generated/prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return [];
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/infra/database/prisma.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Prisma, User } from '@/generated/prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const foundUser = await this.prismaService.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (foundUser) {
+      throw new BadRequestException('Try create again');
+    }
+
+    console.log(data);
+    return this.prismaService.user.create({
+      data,
+    });
+  }
+}
