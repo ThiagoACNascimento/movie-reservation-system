3032b11f79055a7ce02123f2f6fb8615bb7f7430
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-06-30 23:42:08 -0400

    feat: add initial user service test

---
+++ test/unit/users/users.service.spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../../../src/modules/users/users.service';
+import { PrismaService } from '../../../src/infra/database/prisma.service';
+import bcrypt from 'bcrypt';
+
+describe('UserService', () => {
+  let userService: UsersService;
+  const prismaMock = {
+    user: { create: jest.fn(), findUnique: jest.fn(), findMany: jest.fn() },
+  };
+
+  const userInputValue = {
+    name: 'New User',
+    email: 'newuser@example.com',
+    password: '12345678',
+  };
+
+  const userOutputValue = {
+    id: '6153219f-1aca-44b8-940e-cd90922f6ed8',
+    name: 'New User',
+    email: 'newuser@example.com',
+    role: 'default',
+    created_at: '2026-06-29T22:33:57.423Z',
+    updated_at: '2026-06-29T22:33:57.423Z',
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: prismaMock },
+      ],
+    }).compile();
+
+    userService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined();
+  });
+
+  describe('create user (POST)', () => {
+    it('should create a new user', async () => {
+      prismaMock.user.findUnique.mockResolvedValue(null);
+      prismaMock.user.create.mockResolvedValue(userOutputValue);
+      const result = await userService.create(userInputValue);
+
+      expect(result).toEqual(userOutputValue);
+
+      expect(prismaMock.user.create).toHaveBeenCalledWith({
+        data: userInputValue,
+      });
+    });
+  });
+});
