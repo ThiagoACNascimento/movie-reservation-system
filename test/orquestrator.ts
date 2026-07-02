7ffcebd5ec70e3f7431f320ee337b081b927c719
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-01 20:43:38 -0400

    feat: add `userCreate` property and return function values

--- test/orquestrator.ts
+++ test/orquestrator.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from '../src/generated/prisma/client';
+import { PrismaClient, User } from '../src/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { execSync } from 'child_process';
 import bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
+
+interface UserCreateInterface {
+  name?: string;
+  email?: string;
+  password?: string;
+}
 
 export class Orquestrator extends PrismaClient {
   constructor() {
@@ -11,7 +18,7 @@ export class Orquestrator extends PrismaClient {
     super({ adapter, omit: { user: { password: true } } });
   }
 
-  async resetPrismaDatabase() {
+  async resetPrismaDatabase(): Promise<void> {
     await this.$connect();
     execSync('npx prisma migrate reset --force', {
       env: { ...process.env, DATABASE_URL: process.env.DATABASE_URL },
@@ -19,12 +26,13 @@ export class Orquestrator extends PrismaClient {
     });
   }
 
-  async createNewUser() {
-    const hashedPassoword = await bcrypt.hash('12345678', 1);
+  async createNewUser(userCreate?: UserCreateInterface): Promise<User> {
+    const password = userCreate?.password ?? faker.internet.password();
+    const hashedPassoword = await bcrypt.hash(password, 1);
     return this.user.create({
       data: {
-        name: 'newUserCreated',
-        email: 'newUser@gmail.com',
+        name: userCreate?.name ?? faker.person.fullName(),
+        email: userCreate?.password ?? faker.internet.email(),
         password: hashedPassoword,
       },
     });
