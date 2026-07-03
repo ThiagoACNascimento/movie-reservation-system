eb216c5375f95b28bd0730570e382b805def97fe
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-03 19:23:13 -0400

    feat: add `ApiProperty`

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,13 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'The user name',
+    default: 'firstNewUser',
+    minLength: 10,
+  })
   @IsString()
   @MinLength(10)
   name!: string;
 
+  @ApiProperty({
+    description: 'The user email',
+    default: 'newUser@gmail.com',
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    description: 'The user password',
+    minLength: 8,
+    default: '12345678',
+  })
   @IsString()
   @MinLength(8)
   password!: string;
