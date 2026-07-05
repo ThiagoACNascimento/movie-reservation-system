3aa3fd12f3c983b28c5c0b62390a86ec55e7324d
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-04 20:35:35 -0400

    feat: add `public` decorator

--- src/modules/status/status.controller.ts
+++ src/modules/status/status.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { StatusService } from './status.service';
 import { PostgresStatus } from './interfaces/postgres/postgres.interface';
+import { Public } from '../../common/decorators/public.decorator';
 
+@Public()
 @Controller('status')
 export class StatusController {
   constructor(private readonly statusService: StatusService) {}
