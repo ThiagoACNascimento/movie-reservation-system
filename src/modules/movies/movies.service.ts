8eaafd28c7976c42ccd55e27fb98a1ad66b3ea8e
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-10 12:40:53 -0400

    feat: add inital movie service

---
+++ src/modules/movies/movies.service.ts
@@ -0,0 +1,30 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../infra/database/prisma.service';
+import { Movie, Prisma } from '../../generated/prisma/client';
+
+@Injectable()
+export class MoviesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(data: Prisma.MovieCreateInput): Promise<Movie> {
+    const movieExists = await this.getBySlug(data.slug);
+
+    if (movieExists) {
+      throw new BadRequestException('The movie aready exists!');
+    }
+
+    return this.prismaService.movie.create({ data });
+  }
+
+  async getBySlug(slug: string): Promise<Movie> {
+    const movie = await this.prismaService.movie.findUnique({
+      where: { slug },
+    });
+
+    if (!movie) {
+      throw new BadRequestException();
+    }
+
+    return movie;
+  }
+}
