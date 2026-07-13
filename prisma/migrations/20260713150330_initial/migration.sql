5e7eebc8f1cc985719882f69b7fa230af8b555da
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-13 11:12:56 -0400

    chore: delete and rebuild prisma migrations

---
+++ prisma/migrations/20260713150330_initial/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'default');
+
+-- CreateEnum
+CREATE TYPE "Classification" AS ENUM ('L', '6', '10', '12', '14', '16', '18');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(256) NOT NULL,
+    "email" VARCHAR(256) NOT NULL,
+    "password_hash" VARCHAR(72) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'default',
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(256) NOT NULL,
+    "original_title" VARCHAR(256) NOT NULL,
+    "slug" VARCHAR(256) NOT NULL,
+    "release_date" TIMESTAMPTZ NOT NULL,
+    "status" VARCHAR(50) NOT NULL,
+    "score" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "duration" INTEGER NOT NULL,
+    "poster_url" VARCHAR(256),
+    "classification" "Classification" NOT NULL,
+    "gender" TEXT[],
+    "synopsis" VARCHAR(4000) NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Movie_slug_key" ON "Movie"("slug");
