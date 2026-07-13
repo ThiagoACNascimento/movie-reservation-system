6fe8e57d1bbc95fbaad7740f26f124726ec49cc5
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-13 11:11:49 -0400

    chore: remove `jest-setup-env.ts` file

--- test/jest-setup-env.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'path';
-import { config } from 'dotenv';
-import { expand } from 'dotenv-expand';
-
-const env = config({
-  path: resolve(__dirname, '../.env.test'),
-  override: true,
-});
-expand(env);
