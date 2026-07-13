eb3ae944be988e495423f1c9077824ded2f2a335
ThiagoAntonio <thiagoacnascimento@gmail.com>
2026-07-13 09:54:57 -0400

    feat: add `ApiProperty` in `CreateMovieDto`

--- src/modules/movies/dtos/create-movie/create-movie.dto.ts
+++ src/modules/movies/dtos/create-movie/create-movie.dto.ts
@@ -12,43 +12,80 @@ import {
   MinLength,
 } from 'class-validator';
 import { Classification } from '../../../../generated/prisma/enums';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMovieDto {
   @IsString()
   @MinLength(10)
   @MaxLength(256)
+  @ApiProperty({
+    description: 'The Movie Title',
+    example: 'Movie Title',
+  })
   title!: string;
 
   @IsString()
   @MinLength(10)
   @MaxLength(256)
+  @ApiProperty({
+    description: 'The Movie Original Title',
+    example: 'Original title',
+  })
   originalTitle!: string;
 
   @IsString()
   @MinLength(5)
   @MaxLength(50)
+  @ApiProperty({
+    description: 'The Movie Status',
+    example: 'Published',
+  })
   status!: string;
 
   @IsDate()
   @Type(() => Date)
+  @ApiProperty({
+    description: 'The Movie Release Date',
+    example: '2026-07-10T12:02:00.000-04:00',
+  })
   releaseDate!: Date;
 
   @IsNumber()
   @Max(10)
   @Min(0)
+  @ApiProperty({
+    description: 'The Movie Score',
+    example: 5,
+  })
   score!: number;
 
   @IsInt()
+  @ApiProperty({
+    description: 'The Movie Duration',
+    example: 1000 * 60 * 60 * 2,
+  })
   duration!: number;
 
   @IsEnum(Classification)
+  @ApiProperty({
+    description: 'The Movie Classification',
+    example: Classification.L,
+  })
   classification!: Classification;
 
   @IsArray()
+  @ApiProperty({
+    description: 'The Movie Gender',
+    example: 'Action',
+  })
   gender!: string[];
 
   @IsString()
   @MinLength(10)
   @MaxLength(256)
+  @ApiProperty({
+    description: 'The Movie Synopsis',
+    example: 'The best movie ever',
+  })
   synopsis!: string;
 }
