0c7169c57f4f219f2bf84e26057edfb8b9345c67
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-07-16 14:23:48 -0400

    feat: add gender in `UpdateDtoMovie`

--- src/modules/movies/dtos/update-movie/update-movie.dto.ts
+++ src/modules/movies/dtos/update-movie/update-movie.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from '../create-movie/create-movie.dto';
+import { ArrayMinSize, IsArray, IsOptional, IsString } from 'class-validator';
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  @IsOptional()
+  @IsArray()
+  @ArrayMinSize(1, { message: 'The movie has to have at list 1 gender' })
+  @IsString({ each: true })
+  gender?: string[];
+}
