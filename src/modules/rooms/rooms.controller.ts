1b3060e7d00f290e03841eb17b587d46c5e4f7fc
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-07-18 15:09:42 -0400

    feat: add `create`, `findOneBySlug` and `findAll` routes

--- src/modules/rooms/rooms.controller.ts
+++ src/modules/rooms/rooms.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { RoomsService } from './rooms.service';
+import { CreateRoomDto } from './dto/create-room.dto.ts/create-room.dto';
+import { Public } from '../../common/decorators/public.decorator';
+import { Room } from '../../generated/prisma/client';
+import { PaginationDto } from '../../common/dtos/pagination.dto';
+import { PaginationResult } from '../../common/interfaces/pagination-result.interface';
 
+@Public()
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createRoomDto: CreateRoomDto): Promise<Room> {
+    return this.roomsService.create(createRoomDto);
+  }
+
+  @Get(':slug')
+  @HttpCode(HttpStatus.OK)
+  findOneBySlug(@Param('slug') slug: string): Promise<Room> {
+    return this.roomsService.findOneBySlug(slug);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(@Query() pagination: PaginationDto): Promise<PaginationResult<Room>> {
+    return this.roomsService.findAll(pagination);
+  }
 }
