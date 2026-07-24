7e0b66c0928424cf03aee75705460d5937b9cf6d
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-07-24 14:31:20 -0400

    feat: add inital `create` method

--- src/modules/reservations/reservations.controller.ts
+++ src/modules/reservations/reservations.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 
 @Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create() {
+    return [];
+  }
 }
